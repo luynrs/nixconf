d7194a86aabe812cb21ee8ba95d599da307a6473
luynrs <157303229+luynrs@users.noreply.github.com>
2026-08-04 09:08:48 +0300

    -hypr err.

--- nixos/features/hyprland/default.nix
+++ nixos/features/hyprland/default.nix
@@ -12,7 +12,7 @@ in
       monitors = lib.mapAttrsToList (name: m: {
         output = name;
         mode = "${toString m.width}x${toString m.height}@${toString m.refreshRate}";
-        position = "${toString m.x},${toString m.y}";
+        position = "${toString m.x}x${toString m.y}";
         scale = 1;
       }) (lib.filterAttrs (_: m: m.enabled) config.preferences.monitors);
       layouts = lib.concatStringsSep "," config.preferences.keymap.layouts;
