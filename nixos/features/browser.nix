d71aec859238b5a23efd9f700d6d245bb7ad512b
luynrs <157303229+luynrs@users.noreply.github.com>
2026-08-18 20:37:25 +0300

    dark dark dark

--- nixos/features/browser.nix
+++ nixos/features/browser.nix
@@ -1,47 +1,8 @@
 {
   flake.homeModules.firefox =
-    { pkgs, lib, ... }:
-    let
-      caelestiafoxId = "caelestiafox@caelestia.org";
-
-      caelestiafoxXpi =
-        pkgs.runCommand "caelestiafox-xpi"
-          {
-            nativeBuildInputs = [ pkgs.zip ];
-            passthru.addonId = caelestiafoxId;
-          }
-          ''
-            mkdir -p "$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
-            cp -r ${./caelestiafox/extension} extension
-            chmod -R u+w extension
-            cd extension
-            zip -r -X "$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/${caelestiafoxId}.xpi" manifest.json dist
-          '';
-
-      caelestiafoxApp =
-        pkgs.runCommand "caelestiafox-native-app" { nativeBuildInputs = [ pkgs.makeWrapper ]; }
-          ''
-            install -Dm755 ${./caelestiafox/native-app.fish} $out/bin/caelestiafox
-            wrapProgram $out/bin/caelestiafox \
-              --prefix PATH : ${
-                lib.makeBinPath [
-                  pkgs.fish
-                  pkgs.jq
-                  pkgs.inotify-tools
-                ]
-              }
-          '';
-    in
+    { pkgs, ... }:
     {
       home.sessionVariables.MOZ_ENABLE_WAYLAND = "1";
-
-      home.file.".mozilla/native-messaging-hosts/caelestiafox.json".text = builtins.toJSON {
-        name = "caelestiafox";
-        description = "Native app for CaelestiaFox extension.";
-        path = "${caelestiafoxApp}/bin/caelestiafox";
-        type = "stdio";
-        allowed_extensions = [ caelestiafoxId ];
-      };
 
       programs.firefox = {
         enable = true;
@@ -60,25 +21,21 @@
             "network.proxy.socks_remote_dns" = true;
             "network.proxy.no_proxies_on" = "localhost,127.0.0.1";
 
-            "privacy.sanitize.sanitizeOnShutdown" = true;
-            "privacy.clearOnShutdown.cookies" = false;
-            "privacy.clearOnShutdown.sessions" = false;
-
-            "layout.css.prefers-color-scheme.content-override" = 1;
+            "layout.css.prefers-color-scheme.content-override" = 2;
 
             "browser.startup.page" = 3;
             "browser.sessionstore.resume_session_once" = false;
+
+            "middlemouse.paste" = false;
           };
 
           extensions = {
             force = true;
-            packages =
-              (with pkgs.nur.repos.rycee.firefox-addons; [
-                ublock-origin
-                privacy-badger
-                tab-session-manager
-              ])
-              ++ [ caelestiafoxXpi ];
+            packages = with pkgs.nur.repos.rycee.firefox-addons; [
+              ublock-origin
+              privacy-badger
+              tab-session-manager
+            ];
           };
 
           search = {
